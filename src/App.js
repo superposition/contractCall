86e996e1cccaa927e95c0ae6e9f34677dcbb6c0f
Eric Manganaro <ericmanganaro@gmail.com>
2019-02-11 04:12:14 -0500

    --wip-- [skip ci]

--- src/App.js
+++ src/App.js
@@ -2,42 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import styled, { createGlobalStyle } from "styled-components";
-
-import Sidebar from "./components/Sidebar";
-import Content from "./components/Content";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-const GlobalStyle = createGlobalStyle`
-  body, html {
-    margin: 0;
-    padding: 0;
-  }
-`;
-
-const Main = styled.main`
-
-  display: grid;
-  grid-template-columns: repeat(15, 1fr);
-  grid-template-rows: repeat(12, 1fr);
-  border: solid 2px red;
-`;
-
 class App extends Component {
   state={name:"display this here"}
   render() {
     return (
       <div className="App">
-          <GlobalStyle />
-          <Main>
-            <Header>Header</Header>
-            <Content>Content</Content>
-            <Sidebar>
-            </Sidebar>
-            <Footer>Footer</Footer>
-          </Main>
-        
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <h1>{this.state.name}</h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body, html {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const Main = styled.main`
+
+  display: grid;
+  grid-template-columns: repeat(15, 1fr);
+  grid-template-rows: repeat(12, 1fr);
+  border: solid 2px red;
+`;
+
+const Sidebar = styled.div`
+  grid-area: h;
+  border: solid 1px blue;
+  grid-area: 1/1/1/18;
+  padding: 15px;
+  text-align: right;
+`;
+
+const Header = styled.div`
+  grid-area: h;
+  border: solid 1px blue;
+  grid-area: 1/1/1/18;
+  padding: 15px;
+  text-align: right;
+`;
+
+const Content = styled.div`
+  grid-area: 2/1/15/16;
+  border: solid 2px indigo;
+  padding: 15px;
+  backgroundcolor: pink;
+`;
+
+const Footer = styled.div`
+  grid-area: footer;
+  border: solid 2px green;
+  padding: 15px;
+  grid- area: 12 / 2 / 13 / 7;
+`;
+
+
+function App() {
+  
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <Main>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Sidebar>
+        </Sidebar>
+        <Footer>Footer</Footer>
+      </Main>
+    </React.Fragment>
+  )
+  
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
